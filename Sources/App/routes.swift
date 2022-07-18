653278beebf410d35c703be47c711e09fb3eefb9
earmand <dan.earman@gmail.com>
2022-07-18 09:33:07 -0400

    status route

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,3 +1,4 @@
+import Foundation
 import Vapor
 
 func routes(_ app: Application) throws {
@@ -7,5 +8,16 @@ func routes(_ app: Application) throws {
 
     app.get("hello") { req -> String in
         return "Hello, world!"
+    }
+    
+    app.get("dan") { req -> String in
+        return "Hello, Dan!"
+    }
+    
+    app.get("status") { req -> String in
+        
+        let response = String(format:"RefTime:%0.3f",Date.timeIntervalSinceReferenceDate)
+        
+        return response
     }
 }
