898375c01bbfe885fc110ac5022229b68a39bc2b
earmand <dan.earman@gmail.com>
2022-07-18 09:05:13 -0400

    V3

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -4,8 +4,8 @@ import Vapor
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
- 
-    
+    // Test update
+     
     //    app.http.server.configuration.hostname = "10.1.1.137"
     app.http.server.configuration.hostname = "0.0.0.0"
     app.http.server.configuration.port = 9980
